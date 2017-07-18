f77d7fa76d6790674327a6f3e4c108380351d770
Eulogio Gallo <egallo@broadcenter.org>
2017-07-18 13:22:56 -0700

    hmm

--- web/index.php
+++ web/index.php
@@ -1,7 +1,10 @@
 <?php
- require_once('Schoology_OAuth.php');
- require('../vendor/autoload.php');
- //ini_set('memory_limit','10M');
+require_once('Schoology_OAuth.php');
+require('../vendor/autoload.php');
+ini_set('post_max_size', '100M');
+ini_set('upload_max_filesize', '100M');
+//ini_set('memory_limit', '1000M');
+ini_set('max_execution_time', '1920');
  
  
  // establish connection
